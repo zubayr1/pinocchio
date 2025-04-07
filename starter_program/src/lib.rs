925a80110200c12dd54c3b5059fd945e80d16707
zubayr1 <write2zubayr@gmail.com>
2025-04-07 21:32:32 +0200

    entrypoint basic

---
+++ starter_program/src/lib.rs
@@ -0,0 +1,14 @@
+use pinnocchilo::{
+    account_info::AccountInfo, entrypoint, msg, program_error::ProgramError, pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    msg!("Hello, world!");
+    Ok(())
+}
